fdf448f609863142530897db04f2a45dbcb381d0
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-11-18 14:11:26 +0700

    feat: added questions

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -2,9 +2,19 @@ import streamlit as st
 
 
 def main():
-    st.header("Assignment Name")
-    st.subheader("Class Name")
+    st.header("Knee Joint Model Parameter Identification")
+    st.subheader("Biocybernetics")
     st.markdown("Aditya Wardianto 07311940000001")
+    st.markdown(
+        """
+                Knee joint model parameter identification, program development:
+                1.	Motion equation of knee joint (with passive joint parameters).
+                2.	Expression of model parameter identification (objective functions, and gradients)
+                3.	Skeleton program of model simulation of movement (RKIV integration method, visualization)
+                4.	Optimization program to identify the parameters
+                5.	Integration program
+                """
+    )
     st.markdown("Made with ❤️, 🦾,  and ☕")
 
 
