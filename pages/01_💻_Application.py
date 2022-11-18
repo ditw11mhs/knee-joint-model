48f072eb9620544141ca02c24b1ed5ba175ddcf9
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-11-18 14:11:40 +0700

    feat: added initial data exploration

--- pages/01_💻_Application.py
+++ pages/01_💻_Application.py
@@ -5,6 +5,11 @@ from api import logic, utils
 
 def main():
     st.header("App")
+    file = st.file_uploader("Upload file here")
+    if file is not None:
+        df = utils.open_file(file)
+        st.dataframe(df)
+        st.line_chart(df, x="Time", y="Angle")
 
 
 if __name__ == "__main__":
